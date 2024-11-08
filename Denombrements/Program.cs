22cacb9ec5631a5af16436a8fab713719b5ac26b
aurelied <aurelie.demange1991@gmail.com>
2024-11-08 15:16:07 +0100

    Modifications concernant la présentation du code

--- Denombrements/Program.cs
+++ Denombrements/Program.cs
@@ -19,9 +19,10 @@ namespace Denombrements
                 Console.WriteLine("Quitter .......................... 0");
                 Console.Write("Choix :                            ");
                 c = int.Parse(Console.ReadLine());
-
-                if (c == 0) { Environment.Exit(0); }
-
+                if (c == 0) 
+                { 
+                    Environment.Exit(0); 
+                }
                 if (c == 1)
                 {
                     Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
@@ -29,7 +30,9 @@ namespace Denombrements
                                                            // calcul de r
                     long r = 1;
                     for (int k = 1; k <= n; k++)
+                    {
                         r *= k;
+                    }
                     Console.WriteLine(n + "! = " + r);
                 }
                 else
@@ -43,7 +46,9 @@ namespace Denombrements
                         // calcul de r
                         long r = 1;
                         for (int k = (t - n + 1); k <= t; k++)
+                        {
                             r *= k;
+                        }
                         //Console.WriteLine("résultat = " + (r1 / r2));
                         Console.WriteLine("A(" + t + "/" + n + ") = " + r);
                     }
@@ -56,11 +61,15 @@ namespace Denombrements
                         // calcul de r1
                         long r1 = 1;
                         for (int k = (t - n + 1); k <= t; k++)
+                        {
                             r1 *= k;
+                        }
                         // calcul de r2
                         long r2 = 1;
                         for (int k = 1; k <= n; k++)
+                        {
                             r2 *= k;
+                        }
                         // calcul de r3
                         //Console.WriteLine("résultat = " + (r1 / r2));
                         Console.WriteLine("C(" + t + "/" + n + ") = " + (r1 / r2));
